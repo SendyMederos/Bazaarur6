6db3c3182a00ee9ef2b4b0e6428ab545ffb28a7a
SendyMederos <sendymederos@hotmail.com>
2021-01-11 00:35:13 -0500

    to deploy

---
+++ client/src/utils/ProductAPI.js
@@ -0,0 +1,20 @@
+import { Search, SearchRounded } from "@material-ui/icons";
+import axios from "axios";
+
+const path = "/api/products"
+
+export const saveProduct = (productData) => {
+  return axios.post(`${path}`, productData)
+}
+
+export const getProducts = () => {
+  return axios.get(`${path}`)
+}
+
+export const getProduct = (id) => {
+  return axios.get(`${path}/${id}`)
+}
+
+export const deleteProduct = (id) => {
+  return axios.delete(`${path}/${id}`)
+}
